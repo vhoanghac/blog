17e28e8160b53d5f297ab4284dba6eecc50e2620
vhoanghac <stunvn@gmx.com>
2022-06-23 10:23:33 +0700

    Update bo cophieu68

--- header.R
+++ header.R
@@ -1,9 +1,9 @@
-#
+# Packages ####
 require(tidyverse)
 require(data.table)
 require(lubridate)
 
-# 1. Themes:
+# 1. Themes ####
 theme <- theme(
   text = element_text(family = "serif", size = 15),
   
@@ -41,44 +41,46 @@ theme2 <- theme(
   legend.text = element_text(size = 16))
 
 
-# 2. Function lay gia co phieu
+# 2. Function lay gia co phieu ####
 # Lay tu cophieu68: https://www.cophieu68.vn/export/metastock_all.php
+# Update: 06/2022 - phat hien data cophieu68 bi sai sot qua nhieu nen ngung su dung
+# 
+# get_data_co_phieu <- function(ticker = "VNM", all = FALSE){
+#   
+#   raw_data <- fread("01_data/amibroker_all_data.txt", 
+#                     select = c('<DTYYYYMMDD>', '<Ticker>', '<Close>'))
+#   
+#   setnames(raw_data, c("date", "symbol", "price"))
+#   
+#   # Convert number to date
+#   raw_data[, date := lubridate::ymd(date)]
+# 
+#   
+#   # IF ELSE:
+#   # Lua chon tat ca data hoac co phieu bat ky
+#   
+#   # Full stock data:
+#   if(all == TRUE){
+#     
+#     data_tbl <- raw_data %>% 
+#       as_tibble()
+#     
+#     assign("data_tbl", data_tbl, envir = .GlobalEnv)  
+#     
+#   # Be specific:
+#   } else {
+#     
+#     data_symbol <- raw_data[ticker, on = "symbol"] %>% 
+#       as_tibble()
+#     
+#     assign(str_glue("data_{ticker}"), data_symbol, envir = .GlobalEnv)
+#     
+#   }
+#   
+# }
 
-get_data_co_phieu <- function(ticker = "VNM", all = FALSE){
-  
-  raw_data <- fread("01_data/amibroker_all_data.txt", 
-                    select = c('<DTYYYYMMDD>', '<Ticker>', '<Close>'))
-  
-  setnames(raw_data, c("date", "symbol", "price"))
-  
-  # Convert number to date
-  raw_data[, date := lubridate::ymd(date)]
 
-  
-  # IF ELSE:
-  # Lua chon tat ca data hoac co phieu bat ky
-  
-  # Full stock data:
-  if(all == TRUE){
-    
-    data_tbl <- raw_data %>% 
-      as_tibble()
-    
-    assign("data_tbl", data_tbl, envir = .GlobalEnv)  
-    
-  # Be specific:
-  } else {
-    
-    data_symbol <- raw_data[ticker, on = "symbol"] %>% 
-      as_tibble()
-    
-    assign(str_glue("data_{ticker}"), data_symbol, envir = .GlobalEnv)
-    
-  }
-  
-}
-
-# 3. Function import file investing.com
+# 3. Function import file investing.com ####
 
 get_data_investing <- function(data_dir){
   read_csv(data_dir,
@@ -88,9 +90,14 @@ get_data_investing <- function(data_dir){
     janitor::clean_names(.)
 }
 
-# 4. Head Tail data
+
+# 4. Head Tail data ####
 
 head.tail <- function(data){
   print(head(data, 5))
   print(tail(data, 5))
 }
+
+first(mtcars)
+
+
