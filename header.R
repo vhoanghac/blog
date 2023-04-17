af9231852b543e66f97d853998244b10235c7bbc
vhoanghac <30430500+vhoanghac@users.noreply.github.com>
2023-04-17 13:08:06 +0700

    Update function clean data từ vnstock

--- header.R
+++ header.R
@@ -1,7 +1,6 @@
 # Packages ####
-require(tidyverse)
+require(tidyverse) # Tidyverse 2.0 da bao gom lubridate
 require(data.table)
-require(lubridate)
 require(ggtext) #element_markdown() - Improved text rendering for ggplot2
 
 require(showtext) #Custom font
@@ -12,19 +11,19 @@ showtext_auto()
 theme <- theme(
   text = element_text(family = "stix", size = 15),
   
-  title = element_text(color = "#8b0000", size = 20),
+  title = element_text(color = "#8b0000", size = 21),
   
-  plot.title = element_text(hjust = 0.5),
+  plot.title = element_text(hjust = 0.5, margin=margin(0,0,10,0)),
   
   axis.text.y = element_text(face = "bold"),
   
   strip.text = element_text(face = "bold", size = 14),
   
-  plot.subtitle = element_text(hjust = 0.5, size = 16),
+  plot.subtitle = element_markdown(size = 16, color = "#4C4A48", lineheight= 1.2),
   
-  legend.position = "none", # No Legends
+  plot.caption = element_text(size = 14, color = "black", face = "italic"),
   
-  plot.caption = element_text(size = 14, color = "black", face = "italic"))  
+  legend.position = "none") # No Legends
 
 
 
@@ -40,55 +39,24 @@ theme2 <- theme(
   
   strip.text = element_text(face = "bold", size = 14),
   
-  plot.subtitle = element_markdown(size = 15, color = "#4C4A48", lineheight= 1.2),
+  plot.subtitle = element_markdown(size = 16, color = "#4C4A48", lineheight= 1.2),
   
   plot.caption = element_text(size = 14, color = "black", face = "italic"),
   
   legend.key.size = unit(1, 'cm'),
-  legend.text = element_text(size = 16))
+  legend.text = element_text(size = 16),
+  legend.title = element_blank())
 
 
+# 2. Head Tail data ####
+
+head.tail <- function(data){
+  print(head(data, 5))
+  print(tail(data, 5))
+}
 
 
-# 2. Function lay gia co phieu ####
-# Lay tu cophieu68: https://www.cophieu68.vn/export/metastock_all.php
-# Update: 06/2022 - phat hien data cophieu68 bi sai sot qua nhieu nen ngung su dung
-# 
-# get_data_co_phieu <- function(ticker = "VNM", all = FALSE){
-#   
-#   raw_data <- fread("01_data/amibroker_all_data.txt", 
-#                     select = c('<DTYYYYMMDD>', '<Ticker>', '<Close>'))
-#   
-#   setnames(raw_data, c("date", "symbol", "price"))
-#   
-#   # Convert number to date
-#   raw_data[, date := lubridate::ymd(date)]
-# 
-#   
-#   # IF ELSE:
-#   # Lua chon tat ca data hoac co phieu bat ky
-#   
-#   # Full stock data:
-#   if(all == TRUE){
-#     
-#     data_tbl <- raw_data %>% 
-#       as_tibble()
-#     
-#     assign("data_tbl", data_tbl, envir = .GlobalEnv)  
-#     
-#   # Be specific:
-#   } else {
-#     
-#     data_symbol <- raw_data[ticker, on = "symbol"] %>% 
-#       as_tibble()
-#     
-#     assign(str_glue("data_{ticker}"), data_symbol, envir = .GlobalEnv)
-#     
-#   }
-#   
-# }
-
-# 3. Function import file investing.com ####
+# 3. Function import prices from investing.com ####
 
 get_data_investing <- function(data_dir, format = "%b %d, %Y"){
   read_csv(data_dir,
@@ -99,9 +67,26 @@ get_data_investing <- function(data_dir, format = "%b %d, %Y"){
 }
 
 
-# 4. Head Tail data ####
+# 4. Function import prices from tradingview.com ####
 
-head.tail <- function(data){
-  print(head(data, 5))
-  print(tail(data, 5))
+get_data_tradingview <- function(data_dir){
+  
+  read_csv(data_dir) %>% 
+    select(time, contains("close")) %>% 
+    mutate(date = as.Date(time)) %>%
+    janitor::clean_names(.) %>% 
+    rename_with(~str_replace(., "close", "price")) %>% 
+    select(date, contains("price"))
+  
 }
+
+# 5. Function import prices from vnstock
+# Url: https://github.com/thinh-vu/vnstock
+get_data_vnstock <- function(data_dir){
+  
+  read_csv(data_dir) %>% 
+    select(date  = TradingDate,
+           price = Close)
+  
+}
+  
